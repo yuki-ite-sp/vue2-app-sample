e03a9ec30c4a6b2db67bc5e9f67d136eda9d9447
射手 優輝 <yuki_ite@shimapri.jp>
2023-02-06 17:51:29 +0900

    テスト

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <TestKitamuraShopSearch />
+    <KitamuraShopSearch />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import TestKitamuraShopSearch from '@shimauma-print-inc/test-kitamura-shop-search'
+import KitamuraShopSearch from '@shimauma-print-inc/kitamura-shop-search'
+
+console.log(KitamuraShopSearch)
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    TestKitamuraShopSearch
-  }
+    KitamuraShopSearch,
+  },
 }
 </script>
 
