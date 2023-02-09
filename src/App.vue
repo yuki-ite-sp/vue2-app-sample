9d9c365d416d829a30afa2bacfe0fbf8d2177f76
射手 優輝 <yuki_ite@shimapri.jp>
2023-02-09 18:06:43 +0900

    コンポーネント更新

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <KitamuraShopSearch />
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <KitamuraShopSearch
+      cart-api-url="https://aws-cart-api-kitamura.shimaumaprint.com"
+      token="d35121b621635e44e18b015f268651d8c8bb36e1b1bc1accde3c2bdef1f28c90"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import KitamuraShopSearch from '@shimauma-print-inc/kitamura-shop-search'
+import HelloWorld from "./components/HelloWorld.vue";
+import KitamuraShopSearch from "@shimauma-print-inc/kitamura-shop-search";
 
-console.log(KitamuraShopSearch)
+console.log(KitamuraShopSearch);
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
     KitamuraShopSearch,
   },
-}
+};
 </script>
 
 <style>
